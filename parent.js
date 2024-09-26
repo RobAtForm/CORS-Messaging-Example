0e55ba58db36ad3f2f2aef97d41ae06feaf853df
Rob Kinnane <101129378+RobAtForm@users.noreply.github.com>
2024-09-26 17:12:10 -0400

    Update parent.js

--- parent.js
+++ parent.js
@@ -1,6 +1,6 @@
 //This file contains the code that should be run in the parent page
-
-    const allowedOrigin = "https://opx.infotechinc.com/";
+// This code is here as an example: https://app.keysurvey.com/f/41753392/1531/
+    const allowedOrigin = "https://app.form.com";
     window.addEventListener(
         "message",
         async (event) => {
@@ -8,6 +8,6 @@
             
             //This is where you decide what to do with the message from the iframe
             console.log("Received message from child:", event.data);
-            alert($`DataReceived: {event.data}`)
+            alert(`DataReceived: ${event.data}`)
         }
     );
